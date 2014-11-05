bba5a7c90921aa47af8e9f3490f66175c0502567
John Gates <jgates@slac.stanford.edu>
2014-11-05 15:40:17 -0800

    Added dummy schema file for image metadata.

---
+++ sql/winter15_meta.sql
@@ -0,0 +1,14 @@
+REATE TABLE Explosure
+(
+    exposureId BIGINT NOT NULL,
+    llcRa DOUBLE NOT NULL,
+    llcDecl DOUBLE NOT NULL,
+    ulcRa DOUBLE NOT NULL,
+    ulcDecl DOUBLE NOT NULL,
+    urcRa DOUBLE NOT NULL,
+    urcDecl DOUBLE NOT NULL,
+    lrcRa DOUBLE NOT NULL,
+    lrcDecl DOUBLE NOT NULL,
+    PRIMARY KEY (exposureId)
+);
+
